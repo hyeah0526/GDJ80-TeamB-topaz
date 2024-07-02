67a44f48a030ad1783c14df6fc98f4382c4f09d9
hyeah0526 <hyeah0526@naver.com>
2024-07-02 14:07:35 +0900

    first commit

---
+++ topaz/src/main/java/com/topaz/TopazApplication.java
@@ -0,0 +1,13 @@
+package com.topaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopazApplication.class, args);
+	}
+
+}
---
+++ topaz/src/test/java/com/topaz/TopazApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
